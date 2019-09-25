97210b06732387f08beb41f442a979725df368e0
Aufa Ilyasha <55798997+Ailyasha@users.noreply.github.com>
2019-09-25 22:37:07 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan10/logika/PBOIF210118070Latihan10Logika.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan10.logika;
+
+/**
+ *
+ * @author
+ * NAMA     : Aufa Ilyasha
+ * Kelas    : IF2
+ * NIM      : 10118070
+ */
+public class PBOIF210118070Latihan10Logika {
+
+    
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
